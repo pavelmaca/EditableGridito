8b927f68630b187cb84bbd66a4d771758cd27ce0
Pavel Máca <maca.pavel@gmail.com>
2011-06-22 15:19:02 +0200

    SimpleEditableDoctrineModel: init

---
+++ models/SimpleEditableDoctrineModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Gridito;
+
+use Doctrine\ORM\EntityManager;
+
+/**
+ * Simple Doctrine model
+ *
+ * @author Jan Marek
+ */
+class SimpleDoctrineModel extends DoctrineEditableQueryBuilderModel
+{
+	public function __construct(EntityManager $em, $entityName)
+	{
+		parent::__construct($em->getRepository($entityName)->createQueryBuilder("e"));
+		$this->setPrimaryKey($em->getClassMetadata($entityName)->getSingleIdentifierFieldName());
+	}
+}
